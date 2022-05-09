7ab38400e9e8ed849088ce4eba6130b0c204a897
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-05-09 17:29:57 +0200

    Update Uploadfile.py

--- Uploadfile.py
+++ Uploadfile.py
@@ -1,12 +1,15 @@
 import speech_recognition as sr
 import streamlit as st
 import requests   
-r = sr.Recognizer()
-audio_file = st.file_uploader("Upload your audio file here")
-if audio_file is not None: 
-    st.audio(audio_file, format="audio")
-    with sr.AudioFile(audio_file) as source:
-        audio = r.record(source)  
-    recognised_text= r.recognize_google(audio)
-    st.write('The text recognized from the audio seems to be: ')
-    st.write( recognised_text )
+
+
+st.write(st.secrets["my_token"])
+
+AUDIO_FILE=st.file_uploader("upload your audio file")
+if AUDIO_FILE is not None:
+  st.audio(AUDIO_FILE, format="audio/wav")
+  with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # read the entire audio file   
+  recognised_text= r.recognize_google(audio)
+  st.text('the text recognized from the audio seems to be: ')
+  st.text( recognised_text)
