6e9b20beec0f59b788d58729796579f947f1a932
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-05-09 17:08:13 +0200

    Update Uploadfile.py

--- Uploadfile.py
+++ Uploadfile.py
@@ -5,11 +5,8 @@ r = sr.Recognizer()
 audio_file = st.file_uploader("Upload your audio file here")
 if audio_file is not None: 
     st.audio(audio_file, format="audio/wav")
-    
     with sr.AudioFile(audio_file) as source:
         audio = r.record(source)  
-
     recognised_text= r.recognize_google(audio)
-
     st.write('The text recognized from the audio seems to be: ')
     st.write( recognised_text )
